0953d1b7882c246bb378146fcc5b538c43037f1a
Uni App <126077396+cpinnockuni@users.noreply.github.com>
2023-03-01 14:58:11 +0000

    public wall idea

--- uniapi/uniapiapp/models.py
+++ uniapi/uniapiapp/models.py
@@ -1,1 +1,26 @@
+
+"""
+A comminuty public wall where people can post messages 
+
+
+
+"""
+
+
 from django.db import models
+
+
+def bars_resturant():
+    pass 
+
+
+
+def events():
+    pass 
+
+
+
+def checklist_to_uk():
+    pass 
+
+
--- uniapi/uniapiapp/views.py
+++ uniapi/uniapiapp/views.py
@@ -6,3 +6,10 @@ from django.http import HttpResponse
 def index(request):
 
     return HttpResponse(request, "Hello World")
+
+
+
+
+
+def publicwall():
+    pass
